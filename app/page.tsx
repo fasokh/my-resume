233ab17c8ef727904485b6246fdb7862893e1745
fasokh <fatemeh.sokhandan@gmail.com>
2025-11-01 12:59:50 +0330

    resume2025 added

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ const Page: NextPage = () => {
           <p className="text-gray-600 mb-4">Front-End Developer</p>
 
           <a
-            href="/resume.pdf"
+            href="/resume2025.pdf"
             download
             className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-500 transition"
           >
